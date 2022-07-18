439a702da5b33d1dd72acc3a18b9b147833e9e19
RobertoPClaro <robertopacheco1994@gmail.com>
2022-07-18 14:23:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InvertaOsCaracteresDeUmString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o texto que sera invertido: ");
+
+            string novaStr = string.Empty;
+            string strOriginal = Console.ReadLine();
+
+            for (int indice = strOriginal.Length - 1; indice >= 0; indice--)
+            {
+                novaStr += strOriginal[indice];
+            }
+            Console.WriteLine("Texto invertido: ");
+            Console.WriteLine(novaStr);
+            Console.ReadKey();
+        }
+    }
+}
